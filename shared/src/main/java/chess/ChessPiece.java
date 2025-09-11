3832e9d09f4e8807e1a034b8ad5696cb9c5577a9
Joshua <steedjms@gmail.com>
2025-09-10 22:40:32 -0600

    psuedo code for loops and break points to collect all of the potential moves of a bishop as well as additional debug tests

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,9 @@
 package chess;
 
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -55,12 +57,40 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-
-
+        List<ChessMove> possibleMoves = new ArrayList<>();
         ChessPiece piece = board.getPiece(myPosition);
+
         if (piece.getPieceType() == PieceType.BISHOP) {
+            while( true )
+            {
+//                int i = 1;
+                ChessPosition newPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+                // Out of bounds
+                if(newPosition.getRow() < 0 | newPosition.getColumn() < 0 | newPosition.getRow() >= 8 | newPosition.getColumn() >= 8)
+                {
+                    break;
+                }
+
+                // CurrentPos
+                // New Pos
+
+                // If New Pos is:
+                    // out of bounds
+                    // A piece
+                        // Capture
+                        // Denied
+            }
+            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null ));
+            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(2,1), null ));
+            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(2,7), null ));
+            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(3,2), null ));
+
+            System.out.println(myPosition);
+            System.out.println(possibleMoves);
+
             //HARDCODED for FIRST POSITION
-            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null ));
+//            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null ));
+            return possibleMoves;
         }
         return List.of(); // TEMPORARY HACK
         //        throw new RuntimeException("Not implemented");
