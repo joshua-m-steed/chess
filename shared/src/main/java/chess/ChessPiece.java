0fdee2e0f13cd942b84738a2bf755ee9f68874c0
Joshua <steedjms@gmail.com>
2025-09-11 17:03:17 -0600

    impl() recursive method with "out of bounds" base case and moves collection for pos/pos bishop movement

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -61,16 +61,11 @@ public class ChessPiece {
         ChessPiece piece = board.getPiece(myPosition);
 
         if (piece.getPieceType() == PieceType.BISHOP) {
-            while( true )
-            {
-//                int i = 1;
-                ChessPosition newPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
-                // Out of bounds
-                if(newPosition.getRow() < 0 | newPosition.getColumn() < 0 | newPosition.getRow() >= 8 | newPosition.getColumn() >= 8)
-                {
-                    break;
-                }
 
+            calculateBishopMoves(board, myPosition, new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 1), possibleMoves);
+
+
+            // PSUEDO CODE
                 // CurrentPos
                 // New Pos
 
@@ -79,21 +74,35 @@ public class ChessPiece {
                     // A piece
                         // Capture
                         // Denied
-            }
-            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null ));
-            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(2,1), null ));
-            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(2,7), null ));
-            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(3,2), null ));
+//            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null ));
+//            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(2,1), null ));
+//            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(2,7), null ));
+//            possibleMoves.add(new ChessMove(new ChessPosition(5,4), new ChessPosition(3,2), null ));
 
-            System.out.println(myPosition);
             System.out.println(possibleMoves);
 
             //HARDCODED for FIRST POSITION
-//            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null ));
             return possibleMoves;
         }
         return List.of(); // TEMPORARY HACK
         //        throw new RuntimeException("Not implemented");
         //        return new HashSet<ChessMove>(); Another return idea
+    }
+
+    private List<ChessMove> calculateBishopMoves(ChessBoard board, ChessPosition myPos, ChessPosition pos, List<ChessMove> possibleMoves)
+    {
+//        int row = pos.getRow();
+//        int col = pos.getColumn();
+
+        if(pos.getRow() < 0 | pos.getColumn() < 0 | pos.getRow() >= 8 | pos.getColumn() >= 8)
+        {
+            return possibleMoves;
+        }
+
+        System.out.println("RESURSIVE");
+        System.out.println(new ChessMove(myPos, new ChessPosition(pos.getRow(), pos.getColumn()), null ));
+        possibleMoves.add(new ChessMove(myPos, new ChessPosition(pos.getRow(), pos.getColumn()), null ));
+
+        return calculateBishopMoves(board, myPos, new ChessPosition(pos.getRow() + 1,pos.getColumn() + 1 ), possibleMoves);
     }
 }
