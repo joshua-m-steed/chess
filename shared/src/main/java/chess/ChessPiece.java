c2f56ce89c09f94ea681bfa306a7db9f6ee542cf
Joshua <steedjms@gmail.com>
2025-09-11 21:59:35 -0600

    impl(rook) implements the recursive function for the rook movements and passes the tests

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -59,13 +59,22 @@ public class ChessPiece {
         List<ChessMove> possibleMoves = new ArrayList<>();
         ChessPiece piece = board.getPiece(myPosition);
 
-        if (piece.getPieceType() == PieceType.BISHOP) {
+        if(piece.getPieceType() == PieceType.BISHOP) {
             calculateBishopMoves(board, myPosition, new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 1), 1 , 1, possibleMoves);
             calculateBishopMoves(board, myPosition, new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 1), -1 , -1, possibleMoves);
             calculateBishopMoves(board, myPosition, new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 1), 1 , -1, possibleMoves);
             calculateBishopMoves(board, myPosition, new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 1), -1 , 1, possibleMoves);
 //            System.out.println(possibleMoves); // DEBUG PRINT FOR FULL MOVE COLLECTION
         }
+
+        if(piece.getPieceType() == PieceType.ROOK) {
+            calculateRookMoves(board, myPosition, new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn()), 1, 0, possibleMoves);
+            calculateRookMoves(board, myPosition, new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn()), -1, 0, possibleMoves);
+            calculateRookMoves(board, myPosition, new ChessPosition(myPosition.getRow(), myPosition.getColumn() + 1), 0, 1, possibleMoves);
+            calculateRookMoves(board, myPosition, new ChessPosition(myPosition.getRow(), myPosition.getColumn() - 1), 0, -1, possibleMoves);
+//            System.out.println(possibleMoves); // DEBUG PRINT FOR FULL MOVE COLLECTION
+        }
+
         return possibleMoves;
     }
 
@@ -97,6 +106,36 @@ public class ChessPiece {
         possibleMoves.add(new ChessMove(myPos, new ChessPosition(pos.getRow(), pos.getColumn()), null ));
 
         return calculateBishopMoves(board, myPos, new ChessPosition(pos.getRow() + dx,pos.getColumn() + dy), dx, dy, possibleMoves);
+    }
+
+    private List<ChessMove> calculateRookMoves(ChessBoard board, ChessPosition myPos, ChessPosition pos, int dx, int dy,  List<ChessMove> possibleMoves)
+    {
+        if(pos.getRow() < 1 | pos.getColumn() < 1 | pos.getRow() >= 9 | pos.getColumn() >= 9)
+        {
+//            System.out.println("Sorry, out of bounds");
+            return possibleMoves;
+        }
+
+        ChessPiece tile = board.getPiece(pos);
+        ChessPiece myPiece = board.getPiece(myPos);
+        if(tile != null)
+        {
+            if(tile.getTeamColor() != myPiece.getTeamColor())
+            {
+                // SPEAK when you are a piece! ANNOUNCE YOURSELF!
+//                System.out.println("YES, HELLO, I AM NOT NULL");
+                possibleMoves.add(new ChessMove(myPos, new ChessPosition(pos.getRow(), pos.getColumn()), null));
+            }
+            return possibleMoves;
+        }
+
+        // Readability for my pleasure
+//        System.out.println("RECURSIVE");
+//        System.out.println(new ChessMove(myPos, new ChessPosition(pos.getRow(), pos.getColumn()), null ));
+
+        possibleMoves.add(new ChessMove(myPos, new ChessPosition(pos.getRow(), pos.getColumn()), null ));
+
+        return calculateRookMoves(board, myPos, new ChessPosition(pos.getRow() + dx,pos.getColumn() + dy), dx, dy, possibleMoves);
     }
 
     @Override
