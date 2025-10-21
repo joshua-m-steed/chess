d1bff9ef7202239831037469cbf9f19f28da78a1
Joshua <steedjms@gmail.com>
2025-10-21 16:45:27 -0600

    creates a few more tests for login bases to verify that they are handled correctly when given the proper information

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -2,10 +2,12 @@ package service;
 
 import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
+import datamodel.LoginResult;
 import datamodel.RegistrationResult;
 import datamodel.User;
 import io.javalin.http.BadRequestResponse;
 import io.javalin.http.ForbiddenResponse;
+import io.javalin.http.UnauthorizedResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -63,6 +65,58 @@ class UserServiceTest {
 
         assertThrows(BadRequestResponse.class, () -> {
             service.register(missingMail);
+        });
+    }
+
+    @Test
+    void login() {
+        var regUser = new User("James", "haha", "@me.bro");
+
+        var da = new MemoryDataAccess();
+        var service = new UserService(da);
+
+        service.register(regUser);
+        LoginResult res = service.login(regUser);
+
+        assertNotNull(res);
+        assertEquals(res.username(), regUser.username());
+        assertNotNull(res.authToken());
+        assertEquals(String.class, res.authToken().getClass());
+    }
+
+    @Test
+    void loginMissingUserInfo() {
+        var missingUser = new User("", "IS", "");
+        var missingPass = new User("MyName", "", null);
+
+
+        var regUser = new User("MyName", "IS", "@me.bro");
+
+        var da = new MemoryDataAccess();
+        var service = new UserService(da);
+
+        service.register(regUser);
+
+        assertThrows(BadRequestResponse.class, () -> {
+            service.login(missingUser);
+        });
+
+        assertThrows(BadRequestResponse.class, () -> {
+            service.login(missingPass);
+        });
+    }
+
+    @Test
+    void loginUserNotFound() {
+        var regUser = new User("James", "haha", "@me.bro");
+
+        var da = new MemoryDataAccess();
+        var service = new UserService(da);
+
+        service.register(regUser);
+
+        assertThrows(UnauthorizedResponse.class, () -> {
+            service.login(new User("Michael", "uhoh", "Why@you"));
         });
     }
 }
