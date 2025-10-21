f6e5522939acac202583d4ecfe997de4a27ded28
Joshua <steedjms@gmail.com>
2025-10-21 13:18:32 -0600

    creates new negative / exception response test for 403 and 400 error in userService

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -4,7 +4,12 @@ import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
 import datamodel.RegistrationResult;
 import datamodel.User;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.ForbiddenResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.net.HttpURLConnection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +25,44 @@ class UserServiceTest {
         assertEquals(res.username(), user.username());
         assertNotNull(res.authToken());
         assertEquals(String.class, res.authToken().getClass());
+    }
+
+    @Test
+    void registerNameAlreadyExists() {
+        var existingUser = new User("James", "haha", "@me.bro");
+
+        var da = new MemoryDataAccess();
+        var service = new UserService(da);
+
+        RegistrationResult resOne = service.register(existingUser);
+        assertNotNull(resOne);
+        assertEquals(resOne.username(), existingUser.username());
+        assertNotNull(resOne.authToken());
+
+        assertThrows(ForbiddenResponse.class, () -> {
+            service.register(existingUser);
+        });
+    }
+
+    @Test
+    void registerMissingUserInfo() {
+        var missingUser = new User("", "AlphabetSoup", "My@email.com");
+        var missingPass = new User("I have a name", "", "My@email.com");
+        var missingMail = new User("I have a name", "AlphabetSoup", "");
+
+        var da = new MemoryDataAccess();
+        var service = new UserService(da);
+
+        assertThrows(BadRequestResponse.class, () -> {
+            service.register(missingUser);
+        });
+
+        assertThrows(BadRequestResponse.class, () -> {
+            service.register(missingPass);
+        });
+
+        assertThrows(BadRequestResponse.class, () -> {
+            service.register(missingMail);
+        });
     }
 }
