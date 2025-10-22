29a296e2d8b3235c3cefb7b33287187225e12e79
Joshua <steedjms@gmail.com>
2025-10-22 13:52:38 -0600

    includes createUser positive unit test that verifies response and contents

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DataAccessTest {
     @Test
     void clear() {
-        var user = new User("James", "haha", "@me.bro");
+        var user = new User("Frodo", "theOneRing", "frodo@baggins.com");
         DataAccess da = new MemoryDataAccess();
         assertNull(da.getUser(user.username()));
         da.createUser(user);
@@ -16,5 +16,18 @@ class DataAccessTest {
         da.clearUsers();
         da.clearGames();
         assertNull(da.getUser(user.username()));
+    }
+
+    @Test
+    void createUser() {
+        var user = new User("Frodo", "theOneRing", "frodo@baggins.com");
+        DataAccess da = new MemoryDataAccess();
+        da.clearUsers();
+        da.clearGames();
+        da.createUser(user);
+        assertEquals(user, da.getUser(user.username()));
+        assertEquals(user.username(), da.getUser(user.username()).username());
+        assertEquals(user.password(), da.getUser(user.username()).password());
+        assertEquals(user.email(), da.getUser(user.username()).email());
     }
 }
