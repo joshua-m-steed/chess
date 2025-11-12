2996bc394ca2975236a15ecb39c6078918df69f8
Joshua <steedjms@gmail.com>
2025-11-11 22:26:33 -0700

    completes the help for both logged in and logged out states. Does not yet return logged in state

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -19,14 +19,20 @@ public class ChessClient {
     private String help() {
         if(state == State.LOGGEDOUT) {
             return """
-                    register - to create an account
-                    login - to login and play
-                    quit - to leave chess behind
-                    help - to list possible commands
+                    register <USERNAME> <PASSWORD> <EMAIL>  :♔:  to create an account
+                    login <USERNAME> <PASSWORD>             :♔:  to login and play
+                    quit                                    :♔:  to leave chess behind
+                    help                                    :♔:  to list possible commands
                     """;
         }
         return """
-                he
+                create <NAME>                           :♔:  to create a new game
+                list                                    :♔:  to present all existing games
+                join <ID> [ WHITE | BLACK ]             :♔:  to join an existing game
+                watch <ID>                              :♔:  to watch an existing game
+                logout                                  :♔:  to logout
+                quit                                    :♔:  to leave chess behind
+                help                                    :♔:  to llist possible commands
                 """;
     }
 }
