7110e7cdc1bc2b29ee7866c959a1955cf89e579f
Joshua <steedjms@gmail.com>
2025-11-11 22:59:24 -0700

    constructs a looping system in order to take endless inputs. "quit" is currently not an active base case to leave the loop

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,31 +1,46 @@
 package ui;
 
+import java.util.Scanner;
+
 public class ChessClient {
-    private State state = State.LOGGEDOUT;
+    private State state = State.LOGGED_OUT;
 
     public ChessClient() {}
 
     public enum State {
-        LOGGEDIN,
-        LOGGEDOUT
+        LOGGED_IN,
+        LOGGED_OUT
     }
 
     public void run() {
-        System.out.println("Welcome to Chess!");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_GREEN + "Welcome to Chess!");
         System.out.print(help());
+
+        Scanner scanner = new Scanner(System.in);
+        String result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+        System.out.println();
 
     }
 
+    private void printPrompt() {
+        System.out.print("\n"+ EscapeSequences.SET_TEXT_COLOR_BLUE + "[" + this.state + "] >>> ");
+    }
+
     private String help() {
-        if(state == State.LOGGEDOUT) {
-            return """
+        if(state == State.LOGGED_OUT) {
+            return EscapeSequences.SET_TEXT_COLOR_MAGENTA + """
                     register <USERNAME> <PASSWORD> <EMAIL>  :♔:  to create an account
                     login <USERNAME> <PASSWORD>             :♔:  to login and play
                     quit                                    :♔:  to leave chess behind
                     help                                    :♔:  to list possible commands
                     """;
         }
-        return """
+        return EscapeSequences.SET_TEXT_COLOR_MAGENTA + """
                 create <NAME>                           :♔:  to create a new game
                 list                                    :♔:  to present all existing games
                 join <ID> [ WHITE | BLACK ]             :♔:  to join an existing game
