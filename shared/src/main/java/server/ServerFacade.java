3ff491cd8eb6d2372ec313bb6a42d685fb5af50d
Joshua <steedjms@gmail.com>
2025-11-13 11:34:43 -0700

    impl(ServerFacade) creates buildRequest and makeRequestBody after understanding how it works when referencing petshop

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -16,5 +16,22 @@ public class ServerFacade {
 
     private void helper() {
         System.out.format("YOUR SERVER FACADE CONNECTED AT %s \n", serverUrl);
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        HttpRequest.Builder request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
     }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
 }
