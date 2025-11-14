7d1672911999edb6315b42c7708f55a4494c5749
Joshua <steedjms@gmail.com>
2025-11-13 23:53:37 -0700

    impl(pieces) the pieces are now on the board, but sizing is off

--- client/src/main/java/ui/BoardDisplay.java
+++ client/src/main/java/ui/BoardDisplay.java
@@ -36,14 +36,16 @@ public class BoardDisplay {
 
         for(int i = 0; i < 8; i++)
         {
+            result.append(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + "   ");
             // Pre
             for (int j = 0; j < 8; j++)
             {
                 drawTile(i, j, result);
-//                drawPiece(i, j, result, display);
-//            display[i][j] = null;
+                result.append("   ");
+                drawPiece(i, j, result, display);
             }
             //Post
+            result.append(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + "   ");
             result.append(EscapeSequences.RESET_BG_COLOR + EscapeSequences.RESET_TEXT_COLOR)
                     .append("\n");
         }
@@ -63,5 +65,29 @@ public class BoardDisplay {
         } else {
             result.append(EscapeSequences.SET_BG_COLOR_BLACK);
         }
+    }
+
+    private void drawPiece(int i, int j, StringBuilder result, ChessPiece[][] display) {
+        String piece;
+        if (display[i][j] != null) {
+            if (display[i][j].getTeamColor() == ChessGame.TeamColor.BLACK) {
+                result.append(EscapeSequences.SET_TEXT_COLOR_RED);
+            } else {
+                result.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            }
+
+            piece = switch(display[i][j].getPieceType()) {
+                case KING -> EscapeSequences.WHITE_KING;
+                case QUEEN -> EscapeSequences.WHITE_QUEEN;
+                case ROOK -> EscapeSequences.WHITE_ROOK;
+                case BISHOP -> EscapeSequences.WHITE_BISHOP;
+                case KNIGHT -> EscapeSequences.WHITE_KNIGHT;
+                case PAWN -> EscapeSequences.WHITE_PAWN;
+            };
+        } else {
+            piece = "   ";
+        }
+
+        result.append(piece);
     }
 }
