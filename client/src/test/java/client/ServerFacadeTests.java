f9c91642763ac330880d935b1dc2b067f6b0c6b9
Joshua <steedjms@gmail.com>
2025-11-13 20:51:05 -0700

    impl(clearDB) implements first register test for ServerFacade while also doing small modifications to the test in order to clear the database leading up to testing

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,31 @@
 package client;
 
+import chess.ChessPiece;
+import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+import server.ServerFacade.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static String url;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+
+        url = "http://localhost:" + port;
+        clearDatabase();
     }
 
     @AfterAll
@@ -21,9 +34,38 @@ public class ServerFacadeTests {
     }
 
 
+//    @Test
+//    public void sampleTest() {
+//        Assertions.assertTrue(true);
+//    }
+
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerCorrectInfo() throws Exception {
+        ServerFacade facade = new ServerFacade(url);
+        User newUser = new User("MiniJosh", "TheOneRing", "AnEmail");
+
+        Auth newAuth = facade.register(newUser);
+        Assertions.assertNotNull(newAuth);
+        Assertions.assertInstanceOf(String.class, newAuth.authToken());
+        Assertions.assertEquals(newUser.username(), newAuth.username());
+    }
+
+    private static void clearDatabase() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url + "/db"))
+                    .DELETE()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (!(response.statusCode() / 100 == 2)) {
+                throw new RuntimeException("Failed to clear the database");
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Error while attempting to clear the database", ex);
+        }
     }
 
 }
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -11,7 +11,6 @@ import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.Map;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
