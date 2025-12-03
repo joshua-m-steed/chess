4919db5c0a6ff4dcc1fbdc9c8d16c7ad6e71333c
Joshua <steedjms@gmail.com>
2025-12-02 20:12:44 -0700

    impl(errorMessage) implements the errorMessage onto validation of the gameID. passes bad gameID test and also fixed back-to-back test failure

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -2,6 +2,8 @@ package server.websocket;
 
 import com.google.gson.Gson;
 import dataaccess.DataAccess;
+import datamodel.Game;
+import datamodel.GameListResult;
 import datamodel.User;
 import io.javalin.websocket.*;
 import org.eclipse.jetty.websocket.api.Session;
@@ -11,6 +13,8 @@ import websocket.messages.GameMessage;
 import service.GameService.*;
 import service.UserService.*;
 import websocket.messages.NotificationMessage;
+
+import java.util.ArrayList;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
@@ -48,13 +52,30 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     private void join(UserGameCommand command, Session session) throws Exception {
         User authUser = dataAccess.getAuth(command.getAuthToken());
+        Game game = null;
         if (authUser == null) {
             ErrorMessage errorMessage = new ErrorMessage("Error: Could not find the user. Please try again, or reload!");
             connections.send(session, errorMessage);
             return;
         }
-
-
+        ArrayList<Game> gameList = dataAccess.listGame(command.getAuthToken());
+        if (gameList == null || gameList.size() < command.getGameID()) {
+            ErrorMessage errorMessage = new ErrorMessage("Error: Could not find the game. Please try again!");
+            connections.send(session, errorMessage);
+            return;
+        } else {
+            for (Game gameItem : gameList) {
+                if (gameItem.gameID() == command.getGameID());
+                {
+                    game = gameItem;
+                }
+            }
+            if (game == null) {
+                ErrorMessage errorMessage = new ErrorMessage("Error: Could not find the game. Please try again!");
+                connections.send(session, errorMessage);
+                return;
+            }
+        }
 
 
         connections.add(session);
